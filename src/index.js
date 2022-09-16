f5b0d903d2935f088d7d7c7c193ad44ccc2b7fef
Tad <tadieb@gmail.com>
2022-09-16 12:21:58 +0300

    git commit -m "adding local rep to gethub"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+ import React from 'react';
+ import ReactDOM from 'react-dom'   
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ const App=()=>{
+ 	return(
+<div>
+  <label for = "name" className="label">Enter e-mail </label>
+  <input id ="name" type="text"/>
+  <button style={{backgroundColor:'red',color:'white'}}>Submit</button>
+</div>
+ 		)
+ }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ ReactDOM.render(
+<App />,
+document.querySelector('#root')
+ )
